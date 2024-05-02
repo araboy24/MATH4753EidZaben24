4e745546c0a103973191b8b3635160ea5b024ebb
eidzaben24 <eidzaben24@gmail.com>
2024-05-01 20:48:47 -0500

    done

--- R/first_package_function.r
+++ R/first_package_function.r
@@ -1,12 +1,15 @@
-#' myread
+#' myread2
 #'
 #' @param csv
+#' input file
 #'
-#' @return
+#' @return dataframe
 #' @export
-#'
+#' @importFrom utils read.table
 #' @examples
-myread=function(csv){
+#' myread2("data/fire.rda")
+myread2=function(csv){
+  dird="C:/Users/eidza/OneDrive/Documents/MATH4753/RPACKAGES/MATH4753EidZaben24/"
   fl=paste(dird,csv,sep="")
   read.table(fl,header=TRUE,sep=",")
 }
